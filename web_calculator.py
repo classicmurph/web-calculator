e57fac1fb90b7e71ca51e0df35ac80bbd8d2dcdc
Dean Fitzgerald <classicmurph@gmail.com>
2015-02-17 08:58:00 -0500

    results page not showing

--- web_calculator.py
+++ web_calculator.py
@@ -6,15 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('calculator.html')
 
-@app.route('/result.html', methods=[POST'])
+@app.route('/result.html', methods=['POST', 'GET'])
 def calculate():
     value1 = request.form['value1']
     value2 = request.form['value2']
-    operator = request.form['operator']
+    operator = request.select['operator']
     result = eval(num_one + operand + num_two)
 
-    return render_template('calculation.html', Result=result)
-
+    return render_template('result.html'), RESULT=result
 
 
 
